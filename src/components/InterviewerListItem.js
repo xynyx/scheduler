e396c606653ffae1add5e97d217116e9aaa64803
xynyx <matt.taylr22@gmail.com>
2020-04-13 17:46:25 -0400

    Remove comment from InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,12 +1,3 @@
-/* <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li> */
-
 import React from "react";
 import "components/InterviewerListItem.scss";
 import classNames from "classnames";
