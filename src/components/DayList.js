abfd8d3da2e5086a6906f6a67d79fbcb972aaa6a
xynyx <matt.taylr22@gmail.com>
2020-04-13 15:18:31 -0400

    Create DayList component; add stories & data

---
+++ src/components/DayList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+  return (
+    <>
+      <ul></ul>
+    </>
+  );
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,17 @@ export default function DayListItems(props) {
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0,
   });
+
+  const formatSpots = () => {
+    if (spots === 0) return "no spots remaining";
+    else if (spots === 1) return "1 spot remaining";
+    else return `${spots} spots remaining`;
+  };
+
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining.</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
