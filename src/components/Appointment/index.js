03f3c6408aa36b9e96c5bc230052b561bdeb51cd
xynyx <matt.taylr22@gmail.com>
2020-04-14 19:29:25 -0400

    Add Header/Show/Empty to Appointment comp.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
-  const { time } = props;
-  return <article time={time} className="appointment"></article>;
+  const { time, interview } = props;
+  return (
+    <>
+      <Header time={time} />
+      <article time={time} className="appointment">
+        {interview ? (
+          <Show student={interview.student} interviewer={interview.interviewer.name} />
+        ) : (
+          <Empty />
+        )}
+      </article>
+    </>
+  );
 }
-
 
 /* We can use import Appointment from "components/Appointment"; to import a component from the src/components/Appoinment/index.js module. It is an alternative to import Appointment from "components/Appointment/Appointment";. */
