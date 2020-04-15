a9b32b35b58801e78b5b1c085f9c4cb1a05e10d2
xynyx <matt.taylr22@gmail.com>
2020-04-15 19:55:49 -0400

    Add interviewers API data to app.js Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,16 +6,17 @@ import { getAppointmentsForDay } from "helpers/selectors";
 
 import "components/Application.scss";
 
-export default function Application(props) {
+export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
 
-  const appointments = getAppointmentsForDay(state, state.day);
-
   const setDay = day => setState({ ...state, day });
+  
+  const appointments = getAppointmentsForDay(state, state.day);
   const appointmentList = appointments.map(appointment => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
@@ -24,12 +25,14 @@ export default function Application(props) {
     Promise.all([
       Promise.resolve(axios.get("/api/days")),
       Promise.resolve(axios.get("/api/appointments")),
+      Promise.resolve(axios.get("/api/interviewers")),
     ])
       .then(all => {
         setState(prev => ({
           ...prev,
           days: all[0].data,
           appointments: all[1].data,
+          interviewers: all[2].data,
         }));
       })
       .catch(e => e.stack);
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,12 +1,12 @@
 export function getAppointmentsForDay(state, day) {
   const getSpecificDay = state.days.filter(
-    (specificDay) => specificDay.name === day
+    specificDay => specificDay.name === day
   );
   if (getSpecificDay.length === 0) {
     return [];
   }
   const appointmentList = getSpecificDay[0].appointments;
-  const appointmentsForDay = appointmentList.map((appt) => {
+  const appointmentsForDay = appointmentList.map(appt => {
     for (const appointment in state.appointments) {
       if (appt === Number(appointment)) {
         return state.appointments[appointment];
