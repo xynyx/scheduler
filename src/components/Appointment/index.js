fbf464b98d10b5963f0355caa3f65a940ccb20ef
xynyx <matt.taylr22@gmail.com>
2020-04-16 11:57:35 -0400

    Make index.js  useVisualMode for show/empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,17 +3,27 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const { time, interview } = props;
+  const { time, interview, onAdd, onEdit, onDelete } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <>
       <Header time={time} />
       <article time={time} className="appointment">
-        {interview ? (
-          <Show student={interview.student} interviewer={interview.interviewer.name} />
-        ) : (
-          <Empty />
+        {mode === EMPTY && <Empty onAdd={onAdd} />}
+        {mode === SHOW && (
+          <Show
+            student={interview.student}
+            interviewer={interview.interviewer.name}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
         )}
       </article>
     </>
