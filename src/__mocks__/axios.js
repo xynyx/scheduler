d26ab9fb1b39ed1c3517be42091099dcba5c2db7
xynyx <matt.taylr22@gmail.com>
2020-04-21 14:34:52 -0400

    Test for app delete functionality

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -18,6 +18,12 @@ export default {
       status: 204,
       statusText: "No Content"
     })
+  }),
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
   })
 };
 
