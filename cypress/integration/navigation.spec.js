290540691050ac5894f4c144b7a6d177c418f9f5
xynyx <matt.taylr22@gmail.com>
2020-04-22 09:17:00 -0400

    Cypress testing for navigtation to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,3 +3,12 @@ describe("Navigation", () => {
     cy.visit("/");
   });
 });
+
+describe("Navigation", () => {
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.contains("[data-testid=day]", "Tuesday").click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
